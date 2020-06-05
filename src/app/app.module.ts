188af7bc222abdd59fd5d2ecb0c1ae7705e9dde9
unknown <rchimata@rythmos.com>
2020-06-04 23:05:31 -0500

    service folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EmployeeService } from './employee.service';
+import { EmployeeService } from './services/employee.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
