ae14b05343b61fce749892bf07f57d6d3a547a76
unknown <rchimata@rythmos.com>
2020-07-26 11:55:32 -0500

    added event emitter

--- src/app/customers/components/create-edit-customer/create-edit-customer.component.ts
+++ src/app/customers/components/create-edit-customer/create-edit-customer.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { CustomerHelper } from '../../helpers/customer-helper';
+import { CustomerService } from '../../service/customer.service';
+import { Customer } from '../../model/customer';
 
 @Component({
   selector: 'chimata-shop-create-edit-customer',
@@ -9,21 +11,35 @@ import { CustomerHelper } from '../../helpers/customer-helper';
 })
 export class CreateEditCustomerComponent implements OnInit {
 
+  @Output() newCustomer: EventEmitter<Customer> = new EventEmitter();
   customerForm: FormGroup;
 
-  constructor() { 
-    CustomerHelper.initializeCustomerForm();
+  constructor(private customerService: CustomerService) { 
+    this.customerForm = CustomerHelper.initializeCustomerForm();
   }
 
   ngOnInit(): void {
   }
 
   onCancel() {
-
+    this.customerForm.reset();
   }
 
-  saveCustomer() {
-    
+  async saveCustomer(): Promise<void> {
+    let customer: Customer = {
+      firstName: this.customerForm.controls['firstName'].value,
+      lastName: this.customerForm.controls['lastName'].value,
+      email: this.customerForm.controls['email'].value,
+      status: 1
+    }
+    try {
+      await this.customerService.createCustomer(customer).toPromise();
+      this.newCustomer.emit(customer);
+    }
+    catch (error){
+        console.log('Error' + error)
+    }
+
   }
 
 }
--- src/app/customers/model/customer.ts
+++ src/app/customers/model/customer.ts
@@ -1,5 +1,5 @@
 export class Customer {
-    id: number;
+    id?: number;
     firstName: string;
     lastName: string;
     email: string;
