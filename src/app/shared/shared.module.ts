30ef641455b1d5dac834b8e648796750800b1fad
unknown <rchimata@rythmos.com>
2020-07-27 13:59:24 -0500

    Added shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ], 
+  entryComponents: [ConfirmationDialogComponent]
+})
+export class SharedModule { }
