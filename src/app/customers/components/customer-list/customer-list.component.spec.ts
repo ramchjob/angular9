7e4fefe110d8a4236f3f0b2b46f5da77a2102925
unknown <rchimata@rythmos.com>
2020-08-02 18:02:31 -0500

    test case addition

--- src/app/customers/components/customer-list/customer-list.component.spec.ts
+++ src/app/customers/components/customer-list/customer-list.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { CustomerListComponent } from './customer-list.component';
 import { CustomerService } from '../../service/customer.service';
-import { CustomersModule } from '../../customers.module';
+import { FakeMatIconRegistry } from '@angular/material/icon/testing';
 import { CustomerServiceMock } from '../../service/customer.service.mock';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MaterialModule } from 'src/app/material.module';
@@ -11,13 +11,17 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { configureTestSuite } from 'ng-bullet';
+import { of } from 'rxjs';
+import { MatIconRegistry } from '@angular/material/icon';
 
 describe('CustomerListComponent', () => {
   let component: CustomerListComponent;
   let fixture: ComponentFixture<CustomerListComponent>;
   let customerService: CustomerService;
+  let customerServiceMock : CustomerServiceMock = new CustomerServiceMock();
 
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
       declarations: [ CustomerListComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
@@ -30,18 +34,42 @@ describe('CustomerListComponent', () => {
           ReactiveFormsModule,
           MatSnackBarModule
       ],
-      providers: [ { provide: CustomerService, useClass: CustomerServiceMock } ]
+      providers: [ { provide: CustomerService, useClass: CustomerServiceMock },
+        { provide: MatIconRegistry, useClass: FakeMatIconRegistry } ]
     })
     .compileComponents();
-  }));
+  });
+
+    afterAll(() => {
+        const head: HTMLHeadElement = document.getElementsByTagName('head')[0];
+        const styles: HTMLCollectionOf<HTMLStyleElement> | [] = head.getElementsByTagName('style');
+
+        for (let i: number = 0; i < styles.length; i++) {
+            head.removeChild(styles[i]);
+        }
+    });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CustomerListComponent);
+    customerService = TestBed.inject(CustomerService);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.customers = customerServiceMock.getCustomersData();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the data source',  fakeAsync(() => {
+    spyOn(customerService, 'getCustomers').and.returnValue(of(customerServiceMock.getCustomersData()));
+    component.ngOnInit();
+    tick(100);
+    fixture.detectChanges();
+    expect(customerService.getCustomers).toHaveBeenCalled();
+    expect(component.customers).toEqual(customerServiceMock.getCustomersData());
+    expect(component.customerDataSource.data).toEqual(customerServiceMock.getCustomersData());
+  }));
+
 });
